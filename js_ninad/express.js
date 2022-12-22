f335f726e83b6ddf67247c3ddd8f9723f6eecbbc
Ninad Raut <121052576+ninadraut19@users.noreply.github.com>
2022-12-22 16:11:34 +0530

    deleting files

--- js_ninad/express.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const app=express();
-app.get('/',(req,res) =>{
- res.send("Welcome to webserver");
-});
-app.listen(4000,() =>{
-    res.send("listening to port 4000");
-   });
